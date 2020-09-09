60c3084b0d09ef8c85bfb851f51e7d2753cf8e31
zhangyong <yong.zhang@zlrt.net>
2020-09-09 17:15:16 +0800

    dd

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import logo from './logo192.png'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = 'react';
+const obj = {
+  firstName:'xiaoming',
+  lastName:'xiaohong'
+}
+function a(name){
+  return name.firstName + '' + name.lastName
+}
+const b = <div>aaaaa</div>
+const show = false
+const arr = [1,2,3]
+const jsx = (
+  <div>
+    <div>hello,{name}</div>
+    <div>{a(obj)}</div>
+    {b}
+    {show ? 'aaa':'登录'}
+    {arr.map((item)=>(<li key={item}>{item}</li>))}
+    <img src={logo} alt="" style={{width:'10px',height:'10px'}}/>
+  </div>
+)
+ReactDOM.render(jsx,document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// 基本使用，表达式用{}包裹
+// 函数：
+// jsx对象
+// 条件语句
+// 属性
+// 模块化
